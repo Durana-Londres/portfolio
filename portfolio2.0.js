9c99721ad0bb1da1d60177bb7dde3ccce7c15e2b
Durana-Londres <duranalondres21@gmail.com>
2026-05-30 20:56:37 +0100

    Add files via upload

---
+++ portfolio2.0.js
@@ -0,0 +1,25 @@
+function imagem(){
+  let img = document.getElementById("img")
+  img.src = "img/welcome.png"
+
+}
+function imagem2(){
+  let img = document.getElementById("img")
+  img.src = "img/IMG_20260418_123746.jpg"
+}
+/*
+function tema(){
+ /*  let corpo =  document.getElementsById("corpo")
+  corpo.style.backgroundColor = "white"
+  corpo.style.color = "black"
+  console.log("olaaaaaaaaaaaaaa")
+
+  
+}
+function temaO(){
+    let corpo = document.getElementById("body")
+    corpo.style.backgroundColor = "#0f172a";
+    corpo.style.color = "white"
+
+}
+*/
